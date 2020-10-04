c59c0091c45d9e38d770c5ee2959e38c11a96aa3
Krylov Gregory <kryloffgv@joom.com>
2020-10-04 23:36:31 +0300

    Fix

--- src/main.go
+++ src/main.go
@@ -98,7 +98,7 @@ func (e *Endpoints) getUserIDFromToken(r *http.Request) (primitive.ObjectID, err
 func(e *Endpoints) writeHeaders(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Content-Type", "application/json")
 }
 
